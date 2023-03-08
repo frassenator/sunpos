cc97332b4aa56552b773a96cf99c2f903786ff04
frassenator <80252503+frassenator@users.noreply.github.com>
2023-03-08 09:30:43 +0100

    Update DeviceOrientationControls.js

--- js/DeviceOrientationControls.js
+++ js/DeviceOrientationControls.js
@@ -1,147 +1,117 @@
-const {
-  Euler,
-  MathUtils,
-  Quaternion,
-  Vector3
-} = THREE;
-
-const DeviceOrientationControls = function ( object ) {
-
-	const scope = this;
+THREE.DeviceOrientationControls = function ( object ) {
 
 	this.object = object;
+
 	this.object.rotation.reorder( "YXZ" );
 
-	this.enabled = true;
+	this.freeze = true;
 
 	this.deviceOrientation = {};
+
 	this.screenOrientation = 0;
 
-	this.alphaOffset = 0; // radians
+	this.onDeviceOrientationChangeEvent = function( rawEvtData ) {
 
-	const onDeviceOrientationChangeEvent = function ( event ) {
-
-		scope.deviceOrientation = event;
+		this.deviceOrientation = rawEvtData;
 
 	};
 
-	const onScreenOrientationChangeEvent = function () {
+	this.onScreenOrientationChangeEvent = function() {
 
-		scope.screenOrientation = window.orientation || 0;
+		this.screenOrientation = window.orientation || 0;
 
 	};
 
-	const setObjectQuaternion = function () {
+	this.update = function() {
 
-		const zee = new Vector3( 0, 0, 1 );
+		var compassHeading, fixedAlpha;
+		var alpha, beta, gamma;
 
-		const euler = new Euler();
+		return function () {
 
-		const q0 = new Quaternion();
+			if ( this.freeze ) return;
 
-		const q1 = new Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
+                        //
+                        // iOS compass-calibrated 'alpha' fix
+                        // see: http://lists.w3.org/Archives/Public/public-geolocation/2011Jul/0014.html
+                        //
+                        compassHeading = this.deviceOrientation.webkitCompassHeading || this.deviceOrientation.compassHeading;
+                        if ( compassHeading ) {
 
-		return function ( quaternion, alpha, beta, gamma, orient ) {
+                                // Switch compassHeading back to 'alpha' representation
+                                fixedAlpha = 360 - compassHeading;
 
-			euler.set( beta, alpha, - gamma, "YXZ" );
+                        } else {
 
-			quaternion.setFromEuler( euler ); // orient the device
+                                fixedAlpha = this.deviceOrientation.alpha || 0;
 
-			quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
+                        }
 
-			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) ); // adjust for screen orientation
+                        alpha  = THREE.Math.degToRad( fixedAlpha );                                                      // Z
+			beta   = this.deviceOrientation.beta  ? THREE.Math.degToRad( this.deviceOrientation.beta  ) : 0; // X'
+			gamma  = this.deviceOrientation.gamma ? THREE.Math.degToRad( this.deviceOrientation.gamma ) : 0; // Y''
+			orient = this.screenOrientation       ? THREE.Math.degToRad( this.screenOrientation       ) : 0; // O
 
-		};
+			setObjectQuaternion( this.object.quaternion, alpha, beta, gamma, orient );
+
+		}
 
 	}();
 
-	this.connect = function () {
+	function bind( scope, fn ) {
 
-		onScreenOrientationChangeEvent(); // run once on load
-		
-		alert("Connect launched");
+		return function () {
 
-		// iOS 13+
+			fn.apply( scope, arguments );
 
-		if ( window.DeviceOrientationEvent !== undefined && typeof window.DeviceOrientationEvent.requestPermission === 'function' ) {
+		};
 
-			window.DeviceOrientationEvent.requestPermission().then( function ( response ) {
+	};
 
-				if ( response == 'granted' ) {
+	this.connect = function() {
 
-					window.addEventListener( "orientationchange", onScreenOrientationChangeEvent, false );
-					window.addEventListener( "deviceorientation", onDeviceOrientationChangeEvent, false );
+		this.onScreenOrientationChangeEvent(); // run once on load
 
-				}
+		window.addEventListener( 'orientationchange', bind( this, this.onScreenOrientationChangeEvent ), false );
+		window.addEventListener( 'deviceorientation', bind( this, this.onDeviceOrientationChangeEvent ), false );
 
-			} ).catch( function ( error ) {
+		this.freeze = false;
 
-				console.error( 'THREE.DeviceOrientationControls: Unable to use DeviceOrientation API:', error );
+	};
 
-			} );
+	this.disconnect = function() {
 
-		} else {
+		this.freeze = true;
 
-			window.addEventListener( "orientationchange", onScreenOrientationChangeEvent, false );
-			window.addEventListener( "deviceorientation", onDeviceOrientationChangeEvent, false );
+		window.removeEventListener( 'orientationchange', bind( this, this.onScreenOrientationChangeEvent ), false );
+		window.removeEventListener( 'deviceorientation', bind( this, this.onDeviceOrientationChangeEvent ), false );
+
+	};
+
+	// The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
+
+	setObjectQuaternion = function () {
+
+		var zee = new THREE.Vector3( 0, 0, 1 );
+
+		var euler = new THREE.Euler();
+
+		var q0 = new THREE.Quaternion();
+
+		var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
+
+		return function ( quaternion, alpha, beta, gamma, orient ) {
+
+			euler.set( beta, alpha, - gamma, 'YXZ' );                       // 'ZXY' for the device, but 'YXZ' for us
+
+			quaternion.setFromEuler( euler );                               // orient the device
+
+			quaternion.multiply( q1 );                                      // camera looks out the back of the device, not the top
+
+			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) );    // adjust for screen orientation
 
 		}
 
-		scope.enabled = true;
-
-	};
-
-	this.disconnect = function () {
-
-		window.removeEventListener( "orientationchange", onScreenOrientationChangeEvent, false );
-		window.removeEventListener( "deviceorientation", onDeviceOrientationChangeEvent, false );
-
-		scope.enabled = false;
-
-	};
-
-	this.update = function () {
-		
-		alert("Update launched");
-
-		if ( scope.enabled === false ) return;
-
-		const device = scope.deviceOrientation;
-		
-		
-
-		    if (device) {      
-		      // iOS compass-calibrated 'alpha' patch
-		      // see: http://lists.w3.org/Archives/Public/public-geolocation/2011Jul/0014.html
-		      const heading = device.webkitCompassHeading || device.compassHeading;
-
-		      const alpha = device.alpha || heading
-			  ? MathUtils.degToRad(
-			      heading
-			      ? 360 - heading
-			      : device.alpha || 0) + scope.alphaOffset
-			  : 0; // Z
-			    alert("Alpha: " + alpha);
-
-
-			const beta = device.beta ? MathUtils.degToRad( device.beta ) : 0; // X'
-			const gamma = device.gamma ? MathUtils.degToRad( device.gamma ) : 0; // Y''
-
-			const orient = scope.screenOrientation ? MathUtils.degToRad( scope.screenOrientation ) : 0; // O
-
-			setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
-
-		}
-
-	};
-
-	this.dispose = function () {
-
-		scope.disconnect();
-
-	};
-
-	this.connect();
-	this.update();
+	}();
 
 };
